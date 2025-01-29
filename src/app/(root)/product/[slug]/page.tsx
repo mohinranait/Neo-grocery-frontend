8d98f3ebe21106a3960dce0cddb0f90a147e963f
Mohin Rana <ebrahimit49@gmail.com>
2025-01-30 01:22:11 +0600

    Product Details page designed

--- src/app/(root)/product/[slug]/page.tsx
+++ src/app/(root)/product/[slug]/page.tsx
@@ -8,6 +8,19 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import {
+  BadgeDollarSign,
+  Heart,
+  Minus,
+  Plus,
+  Share2,
+  ShieldCheck,
+  Shuffle,
+  Truck,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import ProductViewSlider from "@/components/sliders/ProductViewSlider";
 
 const ProductPage = () => {
   return (
@@ -31,11 +44,15 @@ const ProductPage = () => {
       </div>
 
       <div className="container">
-        <div className="grid grid-cols-2 gap-2">
-          <div></div>
-          <div className="space-y-5">
+        <div className=" flex-col flex md:grid md:grid-cols-[420px_auto] lg:flex lg:flex-row gap-4">
+          <div className="w-full">
+            <div className="  md:w-[420px] bg-red-500 ">
+              <ProductViewSlider />
+            </div>
+          </div>
+          <div className="space-y-5 flex-grow">
             <div>
-              <h1 className="text-4xl font-semibold ">Wine bottle lantern</h1>
+              <h1 className="text-3xl font-semibold ">Wine bottle lantern</h1>
               <div className="flex items-center">
                 <svg
                   className="w-4 h-4 text-yellow-300 me-1"
@@ -94,6 +111,7 @@ const ProductPage = () => {
               </div>
             </div>
             <hr />
+
             <p className="text-sm text-gray-600">
               Consequat a scelerisque suspendisse vel et eget eu vitae
               adipiscing nibh scelerisque semper cum adipiscing facilisis
@@ -101,11 +119,118 @@ const ProductPage = () => {
               ullam corper metus accumsan. Habitasse a purus nec ipsum a urna ac
               ullamcorper varius metus blandit posuere.
             </p>
+
+            <div>
+              <p className="text-sm text-gray-600">
+                SKU: <span>SDF-454</span>
+              </p>
+              <p className="text-sm text-gray-600">
+                Stock:{" "}
+                <span className="text-green-600 font-semibold">In-stock</span>
+              </p>
+              <p className="text-sm text-gray-600">
+                Type: <span className=" ">Organic</span>
+              </p>
+            </div>
             <div className="flex items-center gap-1">
               <span className="text-2xl font-semibold text-gray-900">
                 $24.00 –
               </span>
-              <del className="text-2xl font-semibold text-gray-400">$40.00</del>
+              <del className="text-xl font-semibold text-gray-400">$40.00</del>
+            </div>
+            <div className="flex flex-wrap items-center gap-3">
+              <div className="rounded gap-1 inline-flex items-center ">
+                <span className="w-10 h-10 rounded-full flex items-center justify-center border bg-slate-200 hover:bg-main   text-gray-900 hover:text-white cursor-pointer">
+                  <Minus className="" size={16} />
+                </span>
+
+                <span className=" w-8 text-center block">12</span>
+
+                <span className="w-10 h-10 rounded-full flex items-center justify-center border bg-slate-200 hover:bg-main   text-gray-900 hover:text-white cursor-pointer">
+                  <Plus className="" size={16} />
+                </span>
+              </div>
+              <Button>Add to cart</Button>
+              <Button
+                variant={"outline"}
+                className="bg-main text-white hover:bg-main hover:text-white te"
+              >
+                Buy Now
+              </Button>
+            </div>
+            <div className="flex items-center xl:gap-3">
+              <Button variant={"link"}>
+                <Heart /> Add wishlist
+              </Button>
+              <Button variant={"link"}>
+                <Shuffle /> Add Compare
+              </Button>
+              <Button variant={"link"} className="ml-auto">
+                <Share2 />
+              </Button>
+            </div>
+            <div>
+              <p className="text-gray-500 text-sm">
+                Brand:
+                <Link
+                  href={"/"}
+                  className="text-gray-500 hover:underline hover:text-gray-700 text-sm"
+                >
+                  Olempic
+                </Link>
+              </p>
+              <p className="text-gray-500 text-sm">
+                Category:
+                <Link
+                  href={"/"}
+                  className="text-gray-500 hover:underline hover:text-gray-700 text-sm"
+                >
+                  Sniccks
+                </Link>
+                ,
+                <Link
+                  href={"/"}
+                  className="text-gray-500 hover:underline hover:text-gray-700 text-sm"
+                >
+                  Sock
+                </Link>{" "}
+              </p>
+            </div>
+          </div>
+          <div className="md:col-span-2 w-full">
+            <div className=" xl:w-[310px] ">
+              <ul className="bg-gray-100 space-y-6 p-10 lg:p-4 xl:p-10 rounded">
+                <li className="items-center flex  gap-2">
+                  <div>
+                    <div className="w-7 text-gray-500">
+                      <Truck />
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-500">
+                    Free Shipping apply to all orders over $100
+                  </p>
+                </li>
+                <li className="items-center flex  gap-2">
+                  <div>
+                    <div className="w-7 text-gray-500">
+                      <ShieldCheck />
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-500">
+                    Free Shipping apply to all orders over $100
+                  </p>
+                </li>
+                <li className="items-center flex  gap-2">
+                  <div>
+                    <div className="w-7 text-gray-500">
+                      <BadgeDollarSign />
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-500">
+                    1 Day Returns if you change your mind
+                  </p>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
---
+++ src/components/sliders/ProductViewSlider.tsx
@@ -0,0 +1,212 @@
+"use client";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+// import required modules
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import Image from "next/image";
+import { Swiper as SwiperClass } from "swiper/types";
+
+const ProductViewSlider = () => {
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperClass | null>(null);
+  return (
+    <React.Fragment>
+      <Swiper
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper2"
+      >
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={420}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+            className="w-auto mx-auto max-h-[400px]"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-2.jpg"
+            className="w-[400px] mx-auto max-h-[400px]"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-3.jpg"
+            className="w-[400px] mx-auto max-h-[400px]"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-4.jpg"
+            className="w-[400px] mx-auto max-h-[400px]"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-5.jpg"
+            className="w-[400px] mx-auto max-h-[400px]"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-6.jpg"
+            className="w-[400px] mx-auto max-h-[400px]"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-7.jpg"
+            className="w-[400px] mx-auto max-h-[400px]"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-8.jpg"
+            className="w-[400px] mx-auto max-h-[400px]"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-9.jpg"
+            className="w-[400px] mx-auto max-h-[400px]"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-10.jpg"
+            className="w-auto mx-auto max-h-[400px]"
+          />
+        </SwiperSlide>
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-2.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-3.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-4.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-5.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-6.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-7.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-8.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-9.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image
+            alt="image"
+            width={400}
+            height={400}
+            src="https://swiperjs.com/demos/images/nature-10.jpg"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </React.Fragment>
+  );
+};
+
+export default ProductViewSlider;
