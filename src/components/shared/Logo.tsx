9c3ca5eda65e87fe83359667edabe230a478b17c
Md. Ebrahim <ebrahimit49@gmail.com>
2025-06-19 04:50:23 +0600

    Change Logo

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,15 +1,24 @@
-import { ShoppingBagIcon } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 const Logo = () => {
   return (
-    <Link href={"/"} className="inline-flex items-center">
-      <span className="w-9 h-9 rounded bg-main flex items-center justify-center">
-        <ShoppingBagIcon className="text-white" size={20} />
-      </span>
-      <span className="text-3xl font-bold text-primary">Shop</span>
-    </Link>
+    <>
+      <Link href={"/"} className="inline-flex items-center">
+        <div className="flex items-center space-x-2">
+          <div className="bg-main text-white p-2 rounded-lg">
+            <span className="text-xl font-bold">
+              <ShoppingCart />{" "}
+            </span>
+          </div>
+          <div>
+            <h1 className="text-2xl font-bold text-emerald-600">FreshMart</h1>
+            <p className="text-xs text-gray-500">Your Fresh Store</p>
+          </div>
+        </div>
+      </Link>
+    </>
   );
 };
 
