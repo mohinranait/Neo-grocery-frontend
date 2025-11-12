d41ae4a52263e0c9e30ac495a8d592d439c40579
Md. Ebrahim <ebrahimit49@gmail.com>
2025-11-12 10:38:10 +0600

    Update LOGO

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { ShoppingCart } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -6,18 +6,20 @@ const Logo = () => {
   return (
     <>
       <Link href={"/"} className="inline-flex items-center">
-        <Image src={"/logo.png"} width={200} height={100} alt="Logo" />
-        {/* <div className="flex items-center space-x-2">
+        {/* <Image src={"/logo.png"} width={200} height={100} alt="Logo" /> */}
+        <div className="flex items-center space-x-2">
           <div className="bg-main text-white p-2 rounded-lg">
             <span className="text-xl font-bold">
               <ShoppingCart />{" "}
             </span>
           </div>
           <div>
-            <h1 className="text-2xl font-bold text-main">FreshMart</h1>
-            <p className="text-xs text-gray-500">Your Fresh Store</p>
+            <h1 className="text-2xl font-bold text-main">
+              Collection<span className="text-gray-950">BD</span>{" "}
+            </h1>
+            <p className="text-xs text-gray-500">brandcollectionbd.com</p>
           </div>
-        </div> */}
+        </div>
       </Link>
     </>
   );
